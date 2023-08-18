1c783ca6cef875fb5de2e8ce1d12dbaa2f51c698
Urch22 <urch39777@gmail.com>
2023-08-18 14:34:47 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * fizz_buzz -  a program that prints the numbers from 1 to 100
+ *
+ * Return: o (Success)
+ *
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 1; k <= 100; k++)
+	{
+		if (k % 3 == 0 && k  % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (k % 5 == 0 && k % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (k % 3  == 0 && k % 5 != 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (k == 1)
+		{
+			printf("%d", k);
+		}
+		else
+		{
+		printf("%d", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
