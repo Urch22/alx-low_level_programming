55e8a619330a0a7e41ff47a74b6f4a7eaee200eb
Urch22 <urch39777@gmail.com>
2023-08-18 06:50:38 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers
+ *
+ * Retur: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int k;
+
+	for (k = '0'; k < 10; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
