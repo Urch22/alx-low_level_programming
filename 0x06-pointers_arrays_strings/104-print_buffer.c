d2edfafb7f8b2dd1c4ab55997b2a268b9afb9e73
Urch22 <urch39777@gmail.com>
2023-08-24 15:12:42 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - function that prints a buffer
+ * parameters:
+ * @b: Parameter 1
+ * @size: Parameter 2
+ * Return: 0
+ */
+void print_buffer(char *b, int size)
+{
+	int m, n, o;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		n = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (m = 0; m < 10; m++)
+		{
+			if (m < n)
+				printf("%02x", *(b + o + m));
+			else
+				printf("  ");
+			if (m % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (m = 0; m < n; m++)
+		{
+			int c = *(b + o + m);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
