53117ca7db926a81aa9abcff63a54bbdbac878c5
Urch22 <urch39777@gmail.com>
2023-09-11 07:14:56 -0700

    0x0D. C - Preprocessor tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
