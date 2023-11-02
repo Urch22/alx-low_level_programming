c5dac62c651caf5c8f35ce94d6d2aab3cd32e0f4
Urch22 <urch39777@gmail.com>
2023-11-02 15:34:45 -0700

    updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -75,44 +75,44 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int len1, len2, pr, add, addl, i, j, k, ca;
-	char *ptr;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	len1 = _checknum(argv, 1), len2 = _checknum(argv, 2);
-	_is_zero(argv), pr = len1 + len2, ptr = malloc(pr + 1);
-	if (ptr == NULL)
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	ptr = _initialize_array(ptr, pr);
-	k = pr - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
 			if (addl > 0)
 			{
-				add = (ptr[k] - '0') + addl;
+				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					ptr[k - 1] = (add / 10) + '0';
-				ptr[k] = (add % 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			i = len1 - 1, j--, addl = 0, ca++, k = pr - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
-			if (ptr[0] != '0')
+			if (nout[0] != '0')
 				break;
-			pr--;
-			free(ptr), ptr = malloc(pr + 1), ptr = _initialize_array(ptr, pr);
-			k = pr - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (ptr[k] - '0') + addl;
-			addl = add / 10, ptr[k] = (add % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	printf("%s\n", ptr);
+	printf("%s\n", nout);
 	return (0);
 }
