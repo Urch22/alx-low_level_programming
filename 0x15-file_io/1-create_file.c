3a8925dfadb60392b0a4ed2c8f3241b625f8b4e0
Urch22 <urch39777@gmail.com>
2023-11-27 12:40:22 -0800

    updating

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,27 +3,28 @@
  * create_file - Function that creates a file
  * @filename: Function parameter 1
  * @text_content: Function parameter 2
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, j, k = 0;
+	int i, j, k;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (k = 0; text_content[k];)
-			k++;
-	}
-	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	j = write(i, text_content, k);
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (i == -1 || j == -1)
+	if (i == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+	for (j = 0; text_content[j]; j++)
+		continue;
+	k = write(i, text_content, j);
+	if (k == -1)
 		return (-1);
 
 	close(i);
-
 	return (1);
 }
